8507e8a450d094353a48ddb8de67f373032012f3
mrFillye <58518377+mrFillye@users.noreply.github.com>
2020-10-17 15:12:33 +0300

    weather app

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState({});
+  const [errors, setErrors] = useState(null);
+
+  const getWeather = async () => {
+    var proxyUrl = "https://cors-anywhere.herokuapp.com/";
+    var url = "https://www.metaweather.com/api/location/2122265";
+
+    await fetch(proxyUrl + url)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result.consolidated_weather.splice(0, 1));
+          console.log(result);
+        },
+        (errors) => {
+          setIsLoaded(true);
+          setErrors(errors);
+        }
+      );
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, []);
+
+  let key;
+  for (key in items) {
+    console.log(items[key]);
+    var item = items[key];
+    var stateName = `${item.weather_state_name}`;
+    var humidity = `${item.humidity}`;
+    var minTemp = Math.round(`${item.min_temp}`);
+    var maxTemp = Math.round(`${item.max_temp}`);
+  }
+
+  if (errors) {
+    return <h1>error</h1>;
+  } else if (!isLoaded) {
+    return <h1>Loading...</h1>;
+  } else {
+    return (
+      <div>
+        <h1>{stateName}</h1>
+        <h1>{humidity}</h1>
+        <h3>
+          {minTemp} - {maxTemp}
+        </h3>
+      </div>
+    );
+  }
+}
+
+export default App;
